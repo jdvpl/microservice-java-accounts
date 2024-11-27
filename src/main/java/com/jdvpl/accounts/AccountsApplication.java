889cff15443d3d757da54c9b169fda970cdcde98
jdsuarez <jdsuarez@fgs.co>
2024-11-26 23:42:41 -0500

    add OpenAPI documentation annotations to AccountsApplication

--- src/main/java/com/jdvpl/accounts/AccountsApplication.java
+++ src/main/java/com/jdvpl/accounts/AccountsApplication.java
@@ -4,8 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(info = @Info(title = "Accounts microservie Rest Api Documentation", version = "v1.0", description = "Documentation for account microservice", contact = @Contact(name = "Juan Daniel", email = "juanda554242@gmail.com", url = "https://portfolio-jdvpl.vercel.app/"), license = @License(name = "apache 2.0")))
 public class AccountsApplication {
 
 	public static void main(String[] args) {

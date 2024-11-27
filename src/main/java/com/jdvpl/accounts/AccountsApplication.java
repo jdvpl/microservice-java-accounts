21e56b7d9e2d3f722b42b68f2eb5d5de1ec05b98
jdsuarez <jdsuarez@fgs.co>
2024-11-26 23:42:55 -0500

    remove unused import for ExternalDocumentation in AccountsApplication

--- src/main/java/com/jdvpl/accounts/AccountsApplication.java
+++ src/main/java/com/jdvpl/accounts/AccountsApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;

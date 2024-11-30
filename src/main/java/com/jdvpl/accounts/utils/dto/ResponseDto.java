33dcc396e515e26d64b5368770994cbdc60efc2c
jdsuarez <jdsuarez@fgs.co>
2024-11-30 11:29:46 -0500

    update ResponseDto schema annotation and change server port in application.yml

--- src/main/java/com/jdvpl/accounts/utils/dto/ResponseDto.java
+++ src/main/java/com/jdvpl/accounts/utils/dto/ResponseDto.java
@@ -4,9 +4,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+@Schema(name = "Response", description = "This class is responsible for holding response details")
 @Data
 @AllArgsConstructor
-@Schema(name = "Response", description = "This class is responsible for holding response details")
 public class ResponseDto {
     @Schema(description = "Status code of the response", example = "201")
     private String statusCode;
